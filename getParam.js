2ccb21b4365a3de90606361c83e07f8be0e44e72
Kevin McGinty <atomic.frameworks@gmail.com>
2013-05-07 22:28:19 -0400

    update

--- getParam.js
+++ getParam.js
@@ -1,6 +1,6 @@
 /*
                         
-       ##    ####     Copyright (c) - Kevin McGinty
+       ##    #####     Copyright (c) - Kevin McGinty
      # _ #  ###        
     #   #  #          AtomicFrameworks
     
